43f07f0fabceaf215f616d0580d95a12e64b8f14
huxian <827818841@qq.com>
2018-07-07 23:29:20 +0800

    用户增加非空判断

--- src/main/resources/static/bsns/sys/user.js
+++ src/main/resources/static/bsns/sys/user.js
@@ -17,9 +17,7 @@ var showColumns = [
     , {
         field: "userName",
         title: "用户名称",
-        width: "10%",
-        sortable: true,
-        sortName: "user_name" // sortName的值，需配置和数据库保持一致
+        width: "10%"
     }
     , {
         field: "nickName",
@@ -92,216 +90,190 @@ var bsTable = new BootStrapTable();
 
 // 定义vue实例
 var vm = new Vue({
-    el: "#" + VUE_EL
-    , data: {
+        el: "#" + VUE_EL
+        , data: {
 
-        /* 定义bootstrap-table表格参数 */
-        queryOption: {}
-        , columns: showColumns
+            /* 定义bootstrap-table表格参数 */
+            queryOption: {}
+            , columns: showColumns
 
-        /* 定义页面操作参数 */
-        , show: true// 切换页面中的查询和新建（编辑）页面
-        , showPwd: true // 显示修改密码框
-        , errorMessage: null // 异常信息
-        , title: null // 标题
-        , vueQueryParam: { // 查询参数
-            keyword: null,
+            /* 定义页面操作参数 */
+            , show: true// 切换页面中的查询和新建（编辑）页面
+            , showPwd: true // 显示修改密码框
+            , errorMessage: null // 异常信息
+            , title: null // 标题
+            , vueQueryParam: { // 查询参数
+                keyword: null,
+            }
+            , model: {} //实体对象(用于新建、修改页面)
+            , roles: [] // 加载角色列表对象
+            , userRoles: [] // 用户选择的角色
+
+            // 定义模块名称
+            , moduleName: "user"
         }
-        , model: {} //实体对象(用于新建、修改页面)
-        , roles: [] // 加载角色列表对象
-        , userRoles: [] // 用户选择的角色
+        // 定义方法
+        , methods: {
 
-        // 定义模块名称
-        , moduleName: "user"
-    }
-    // 定义方法
-    , methods: {
+            // 点击“查询”按钮事件
+            query: function () {
+                vm.reload();
+            }
 
-        // 点击“查询”按钮事件
-        query: function () {
-            vm.reload();
-        }
+            // 点击“新增”按钮
+            , save: function (event) {
+                // 1. 隐藏表格，显示添加页面
+                vm.showPwd = true;
+                vm.show = false;
+                vm.errorMessage = null;
 
-        // 点击“新增”按钮
-        , save: function (event) {
-            // 1. 隐藏表格，显示添加页面
-            vm.showPwd = true;
-            vm.show = false;
-            vm.errorMessage = null;
+                // 2. 设置标题
+                vm.title = PAGE_INSERT_TITLE;
+                // 3. 清空表单数据
+                vm.model = {};
+                vm.userRoles=[];
 
-            // 2. 设置标题
-            vm.title = PAGE_INSERT_TITLE;
-            // 3. 清空表单数据
-            vm.model = {};
+                // 4. 加载角色列表
+                vm.loadRoles();
 
-            // 4. 加载角色列表
-            vm.loadRoles();
+                //5.加载树控件
+                vm.loadTreeCompany('add');
+            }
 
-            //5.加载树控件
-            vm.loadTreeCompany('add');
-        }
+            // 点击“确定”按钮
+            , commit: function (el) {
 
-        // 点击“确定”按钮
-        , commit: function (el) {
-
-            // 用户名
-            if (vm.model.userName == null || vm.model.userName == "") {
-                vm.errorMessage = "请输入用户名";
-                return;
-            }else{
                 // 用户名
-                if (vm.model.userName.trim() == null || vm.model.userName.trim() == "") {
+                if (vm.model.userName == null || vm.model.userName == "") {
                     vm.errorMessage = "请输入用户名";
                     return;
+                } else {
+                    // 用户名
+                    if (vm.model.userName.trim() == null || vm.model.userName.trim() == "") {
+                        vm.errorMessage = "请输入用户名";
+                        return;
+                    } else {
+                        //判断是否重复
+                    }
                 }
-            }
-            // 用户昵称
-            if (vm.model.nickName == null || vm.model.nickName == "") {
-                vm.errorMessage = "请输入用户昵称";
-                return;
-            }else{
-                if (vm.model.nickName.trim() == null || vm.model.nickName.trim() == "") {
+                // 用户昵称
+                if (vm.model.nickName == null || vm.model.nickName == "") {
                     vm.errorMessage = "请输入用户昵称";
                     return;
+                } else {
+                    if (vm.model.nickName.trim() == null || vm.model.nickName.trim() == "") {
+                        vm.errorMessage = "请输入用户昵称";
+                        return;
+                    } else {
+                        if (vm.model.nickName.length > 6) {
+                            vm.errorMessage = "昵称长度不能大于6位";
+                            return;
+                        }
+                    }
                 }
-            }
-            // 密码
-            if (vm.model.password == null || vm.model.password == "") {
-                vm.errorMessage = "请输入密码";
-                return;
-            }else{
-                if (vm.model.password.trim() == null || vm.model.password.trim() == "") {
-                    vm.errorMessage = "请输入密码";
+                if (vm.model.id == null) {
+                    // 密码
+                    if (vm.model.password == null || vm.model.password == "") {
+                        vm.errorMessage = "请输入密码";
+                        return;
+                    } else {
+                        if (vm.model.password.trim() == null || vm.model.password.trim() == "") {
+                            vm.errorMessage = "请输入密码";
+                            return;
+                        } else {
+                            if (vm.model.password.length < 6 || vm.model.password.length > 16) {
+                                vm.errorMessage = "密码长度不能小于6位且不能大于16位";
+                                return;
+                            }
+                        }
+                    }
+                    // 重复密码
+                    if (vm.model.newPassword == null || vm.model.newPassword == "") {
+                        vm.errorMessage = "请输入确认密码";
+                        return;
+                    }
+                    else {
+                        if (vm.model.newPassword.trim() == null || vm.model.newPassword.trim() == "") {
+                            vm.errorMessage = "请输入确认密码";
+                            return;
+                        } else {
+                            if (vm.model.newPassword != vm.model.password) {
+                                vm.errorMessage = "两次密码输入不一致";
+                                return;
+                            }
+                        }
+                    }
+                }
+                // 邮箱
+                if (vm.model.email == null || vm.model.email == "") {
+                    vm.errorMessage = "请输入邮箱";
                     return;
+                } else {
+                    if (vm.model.email.trim() == null || vm.model.email.trim() == "") {
+                        vm.errorMessage = "请输入邮箱";
+                        return;
+                    } else {
+                        // 定义校验规则
+                        var regEmail = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+                        flag = regEmail.test(vm.model.email);
+                        // 规则校验：不符合规则则给出提示
+                        if (!flag) {
+                            vm.errorMessage = "请正确填写邮箱";
+                            return;
+                        }
+                    }
                 }
-            }
-            // 重复密码
-            if (vm.model.newPassword == null || vm.model.newPassword == "") {
-                vm.errorMessage = "请输入确认密码";
-                return;
-            }else{
-                if (vm.model.newPassword.trim() == null || vm.model.newPassword.trim() == "") {
-                    vm.errorMessage = "请输入确认密码";
-                    return;
-                }
-            }
-            // 手机号码
-            if (vm.model.mobile == null || vm.model.mobile == "") {
-                vm.errorMessage = "请输入手机号码";
-                return;
-            }else{
-                if (vm.model.mobile.trim() == null || vm.model.mobile.trim() == "") {
+
+                // 手机号码
+                if (vm.model.mobile == null || vm.model.mobile == "") {
                     vm.errorMessage = "请输入手机号码";
                     return;
-                }
-            }
-
-
-            // 执行新增操作
-            if (vm.model.id == null) {
-                vm.doSave();
-                return;
-            }
-
-            // 执行修改操作
-            vm.doUpdate();
-        }
-
-        // 执行保存操作
-        , doSave: function () {
-
-            // 获取到的用户配置的角色列表添加到后台参数
-            vm.model.userRoles = vm.userRoles;
-
-            // 2. 入库
-            $.ajax({
-                type: "POST",
-                url: APP_NAME + "/sys/" + vm.moduleName + "/insert",
-                contentType: "application/json",
-                data: JSON.stringify(vm.model),
-                success: function (r) {
-                    if (r.code === 0) {
-                        alert(PAGE_OPERATOR_SUCCESS, function (index) {
-                            vm.reload();
-                        });
-                    } else if (r.code) {
-                        alert(r.msg);
+                } else {
+                    if (vm.model.mobile.trim() == null || vm.model.mobile.trim() == "") {
+                        vm.errorMessage = "请输入手机号码";
+                        return;
                     } else {
-                        alert(r.msg);
+                        // 定义校验规则
+                        var regPhone = /^1[3|4|5|7|8]\d{9}$/;
+                        flag = regPhone.test(vm.model.mobile);
+                        // 规则校验：不符合规则则给出提示
+                        if (!flag) {
+                            vm.errorMessage = "请正确填写手机号码";
+                            return;
+                        }
                     }
                 }
-            });
-        }
 
-        // 显示修改页面
-        , update: function () {
-
-            // 隐藏密码框
-            vm.showPwd = false;
-            vm.errorMessage = null;
-
-            // 获取所选择选择数据行的ID（可能选择多行）
-            var ids = bsTable.getMultiRowIds();
-
-            // 校验只能选择一行
-            if (ids.length != 1) {
-                alert(PAGE_SELECT_ONE);
-                return;
-            }
-
-            $.get(APP_NAME + "/sys/" + vm.moduleName + "/" + ids[0], function (r) {
-                vm.show = false;
-                vm.title = PAGE_UPDATE_TITLE;
-                vm.model = r.model;
-            });
-            // 加载角色列表
-            vm.loadRoles();
-
-        }
-
-        // 执行修改操作
-        , doUpdate: function () {
-
-            // 执行修改
-            $.ajax({
-                type: "POST",
-                url: APP_NAME + "/sys/" + vm.moduleName + "/update",
-                contentType: "application/json",
-                data: JSON.stringify(vm.model),
-                success: function (r) {
-                    if (r.code === 0) {
-                        alert(PAGE_OPERATOR_SUCCESS, function (index) {
-                            vm.reload();
-                        });
-                    } else if (r.code) {
-                        alert(r.msg);
-                    } else {
-                        alert(r.msg);
-                    }
+                // 角色
+                if (vm.userRoles == null || vm.userRoles == "") {
+                    vm.errorMessage = "请选择角色";
+                    return;
                 }
-            });
-        }
 
-        // 点击“删除”按钮
-        , del: function (event) {
+                // 执行新增操作
+                if (vm.model.id == null) {
+                    vm.doSave();
+                    return;
+                }
 
-            // 获取选择记录ID
-            var ids = bsTable.getMultiRowIds();
-
-            // 校验未选择任何一行
-            if (ids == null || ids.length <= 0) {
-                alert(PAGE_SELECT_ONE);
-                return;
+                // 执行修改操作
+                vm.doUpdate();
             }
+            // 执行保存操作
+            ,
+            doSave: function () {
 
-            confirm(PAGE_ARE_YOU_SURE_DEL, function () {
+                // 获取到的用户配置的角色列表添加到后台参数
+                vm.model.userRoles = vm.userRoles;
+
+                // 2. 入库
                 $.ajax({
                     type: "POST",
-                    url: APP_NAME + "/sys/" + vm.moduleName + "/delete",
+                    url: APP_NAME + "/sys/" + vm.moduleName + "/insert",
                     contentType: "application/json",
-                    data: JSON.stringify(ids),
+                    data: JSON.stringify(vm.model),
                     success: function (r) {
-                        if (r.code == 0) {
+                        if (r.code === 0) {
                             alert(PAGE_OPERATOR_SUCCESS, function (index) {
                                 vm.reload();
                             });
@@ -312,64 +284,152 @@ var vm = new Vue({
                         }
                     }
                 });
-            });
-        }
+            }
 
-        // 重新加载(ok)
-        , reload: function () {
+            // 显示修改页面
+            ,
+            update: function () {
 
-            // 展示查询列表
-            vm.show = true;
+                // 隐藏密码框
+                vm.showPwd = false;
+                vm.errorMessage = null;
 
-            // 查询条件
-            var queryOpt = {
-                'keyword': vm.vueQueryParam.keyword == null ? "" : vm.vueQueryParam.keyword.trim(),
-            };
+                // 获取所选择选择数据行的ID（可能选择多行）
+                var ids = bsTable.getMultiRowIds();
 
-            vm.queryOption = queryOpt;
+                // 校验只能选择一行
+                if (ids.length != 1) {
+                    alert(PAGE_SELECT_ONE);
+                    return;
+                }
 
-            // 刷新表格数据
-            bsTable.createBootStrapTable(showColumns, APP_NAME + "/sys/" + vm.moduleName + "/list?rnd=" + Math.random(), vm.queryOption);
-        }
+                $.get(APP_NAME + "/sys/" + vm.moduleName + "/" + ids[0], function (r) {
+                    vm.show = false;
+                    vm.title = PAGE_UPDATE_TITLE;
+                    vm.model = r.model;
+                });
+                // 加载角色列表
+                vm.loadRoles();
 
-        // 加载角色列表
-        , loadRoles: function () {
-            $.get(APP_NAME + "/sys/" + vm.moduleName + "/loadRoles", function (r) {
-                vm.roles = r.page;
-            });
-        }
-        , loadTreeCompany: function (type) {
-            function getMenuJson(url, data) {
-                var zNodes;
-                var role = {id: data};
+            }
+
+            // 执行修改操作
+            ,
+            doUpdate: function () {
+
+                // 执行修改
                 $.ajax({
-                    url: url,
-                    dataType: 'JSON',
-                    type: 'POST',
-                    data: role,
-                    async: false,
-                    success: function (data, status) {
-                        var nodes = JSON.stringify(data.model);
-                        zNodes = eval(nodes);
+                    type: "POST",
+                    url: APP_NAME + "/sys/" + vm.moduleName + "/update",
+                    contentType: "application/json",
+                    data: JSON.stringify(vm.model),
+                    success: function (r) {
+                        if (r.code === 0) {
+                            alert(PAGE_OPERATOR_SUCCESS, function (index) {
+                                vm.reload();
+                            });
+                        } else if (r.code) {
+                            alert(r.msg);
+                        } else {
+                            alert(r.msg);
+                        }
                     }
                 });
-                return zNodes;
             }
 
-            if (type == 'add') {
-                var data = null;
-                ztree = $.fn.zTree.init($("#menuTree"), setting, getMenuJson(APP_NAME + "/sys/menu/queryAllMenuInsert", data));
-            }
-            // else if (type=='update'){
-            //     var ids = bsTable.getMultiRowIds();
-            //     var data = ids[0];
-            //     /*ztree = $.fn.zTree.init($("#menuTree"), setting,getMenuJson(APP_NAME + "/sys/menu/queryAllMenuUpdate",data) );*/
-            //     ztree = $.fn.zTree.init($("#menuTree"), setting,getMenuJson(APP_NAME + "/sys/menu/queryAllMenuUpdate",data) );
-            // }
+            // 点击“删除”按钮
+            ,
+            del: function (event) {
 
+                // 获取选择记录ID
+                var ids = bsTable.getMultiRowIds();
+
+                // 校验未选择任何一行
+                if (ids == null || ids.length <= 0) {
+                    alert(PAGE_SELECT_ONE);
+                    return;
+                }
+
+                confirm(PAGE_ARE_YOU_SURE_DEL, function () {
+                    $.ajax({
+                        type: "POST",
+                        url: APP_NAME + "/sys/" + vm.moduleName + "/delete",
+                        contentType: "application/json",
+                        data: JSON.stringify(ids),
+                        success: function (r) {
+                            if (r.code == 0) {
+                                alert(PAGE_OPERATOR_SUCCESS, function (index) {
+                                    vm.reload();
+                                });
+                            } else if (r.code) {
+                                alert(r.msg);
+                            } else {
+                                alert(r.msg);
+                            }
+                        }
+                    });
+                });
+            }
+
+            // 重新加载(ok)
+            ,
+            reload: function () {
+
+                // 展示查询列表
+                vm.show = true;
+
+                // 查询条件
+                var queryOpt = {
+                    'keyword': vm.vueQueryParam.keyword == null ? "" : vm.vueQueryParam.keyword.trim(),
+                };
+
+                vm.queryOption = queryOpt;
+
+                // 刷新表格数据
+                bsTable.createBootStrapTable(showColumns, APP_NAME + "/sys/" + vm.moduleName + "/list?rnd=" + Math.random(), vm.queryOption);
+            }
+
+            // 加载角色列表
+            ,
+            loadRoles: function () {
+                $.get(APP_NAME + "/sys/" + vm.moduleName + "/loadRoles", function (r) {
+                    vm.roles = r.page;
+                });
+            }
+            ,
+            loadTreeCompany: function (type) {
+                function getMenuJson(url, data) {
+                    var zNodes;
+                    var role = {id: data};
+                    $.ajax({
+                        url: url,
+                        dataType: 'JSON',
+                        type: 'POST',
+                        data: role,
+                        async: false,
+                        success: function (data, status) {
+                            var nodes = JSON.stringify(data.model);
+                            zNodes = eval(nodes);
+                        }
+                    });
+                    return zNodes;
+                }
+
+                if (type == 'add') {
+                    var data = null;
+                    ztree = $.fn.zTree.init($("#menuTree"), setting, getMenuJson(APP_NAME + "/sys/menu/queryAllMenuInsert", data));
+                }
+                // else if (type=='update'){
+                //     var ids = bsTable.getMultiRowIds();
+                //     var data = ids[0];
+                //     /*ztree = $.fn.zTree.init($("#menuTree"), setting,getMenuJson(APP_NAME + "/sys/menu/queryAllMenuUpdate",data) );*/
+                //     ztree = $.fn.zTree.init($("#menuTree"), setting,getMenuJson(APP_NAME + "/sys/menu/queryAllMenuUpdate",data) );
+                // }
+
+            }
         }
-    }
-});
+    })
+;
 
 /**
  * 页面初始化执行
