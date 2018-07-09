45ddf267e90ebbe1b3f1554ab234a7c2c8bff6e4
huxian <827818841@qq.com>
2018-07-09 10:35:26 +0800

    增加密码加密方法

--- src/main/java/cn/com/njdhy/muscle/triceps/util/ShiroUtil.java
+++ src/main/java/cn/com/njdhy/muscle/triceps/util/ShiroUtil.java
@@ -3,6 +3,7 @@ package cn.com.njdhy.muscle.triceps.util;
 
 import cn.com.njdhy.muscle.triceps.model.database.SysUser;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.crypto.hash.SimpleHash;
 
 /**
  * <类功能简述> shiro 工具类
@@ -19,6 +20,17 @@ public class ShiroUtil {
     public static String getLoginUserName() {
         SysUser user = (SysUser) SecurityUtils.getSubject().getPrincipal();
         return user.getUserName();
+    }
+
+    /**
+     * 函数功能描述：shiro密码加密
+     * @param user
+     * @return
+     */
+    public static String passwordEncrypr(SysUser user) {
+        //shiro自带的MD5加密
+        SimpleHash hash = new SimpleHash("md5", user.getPassword(), user.getSalt(), 3);
+        return hash.toHex();
     }
 
 
